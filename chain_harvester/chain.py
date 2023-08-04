e215002c93e779aa3d67a432d4e091688bc19d94
0xCommanderKeen <91289621+0xCommanderKeen@users.noreply.github.com>
2023-08-04 22:21:40 +0200

    tests

--- chain_harvester/chain.py
+++ chain_harvester/chain.py
@@ -54,11 +54,7 @@ class Chain:
             session.mount("http://", adapter)
             session.mount("https://", adapter)
 
-            self._chain = Web3(
-                Web3.HTTPProvider(
-                    self.rpc, request_kwargs={"timeout": 60}, session=session
-                )
-            )
+            self._chain = Web3(Web3.HTTPProvider(self.rpc, request_kwargs={"timeout": 60}, session=session))
             self._chain.middleware_onion.inject(geth_poa_middleware, layer=0)
         return self._chain
 
@@ -73,12 +69,17 @@ class Chain:
         return self.eth.get_block_number()
 
     def get_abi_from_etherscan(self, contract_address):
-        req = requests.get(
-            "https://api.etherscan.io/api?module=contract&action=getabi&address="
-            + contract_address
-            + "&apikey="
-            + self.etherscan_api_key
-        )
+        try:
+            req = requests.get(
+                "https://api.etherscan.io/api?module=contract&action=getabi&address="
+                + contract_address
+                + "&apikey="
+                + self.etherscan_api_key,
+                timeout=5,
+            )
+        except requests.exceptions.Timeout:
+            log.exception("Timeout when get abi from etherscan", extra={"contract_address": contract_address})
+            raise
         resp = json.loads(req.text)
         abi = json.loads(resp["result"])
         return abi
@@ -90,18 +91,16 @@ class Chain:
                 os.makedirs(self.abis_path)
             current_file_path = inspect.getfile(inspect.currentframe())
             current_directory = os.path.dirname(os.path.abspath(current_file_path))
-            file_path = os.path.join(
-                current_directory, "abis", f"{contract_address}.json"
-            )
+            file_path = os.path.join(current_directory, "abis", f"{contract_address}.json")
             if os.path.exists(file_path):
-                with open(file_path, "r") as f:
+                with open(file_path) as f:
                     self._abis[contract_address] = json.loads(f.read())
             else:
                 abi = self.get_abi_from_etherscan(contract_address)
 
                 with open(file_path, "w") as f:
                     json.dump(abi, f)
-                with open(file_path, "r") as f:
+                with open(file_path) as f:
                     self._abis[contract_address] = json.loads(f.read())
         return self._abis[contract_address]
 
@@ -115,16 +114,12 @@ class Chain:
         contract_address = Web3.to_checksum_address(contract_address)
         contract = self.get_contract(contract_address)
         contract_function = contract.get_function_by_name(function_name)
-        result = contract_function(*args).call(
-            block_identifier=kwargs.get("block_identifier", "latest")
-        )
+        result = contract_function(*args).call(block_identifier=kwargs.get("block_identifier", "latest"))
         return result
 
     def get_storage_at(self, contract_address, position, block_identifier=None):
         contract_address = Web3.to_checksum_address(contract_address)
-        content = self.eth.get_storage_at(
-            contract_address, position, block_identifier=block_identifier
-        ).hex()
+        content = self.eth.get_storage_at(contract_address, position, block_identifier=block_identifier).hex()
         return content
 
     def _yield_events(self, fetch_events_func, from_block, to_block=None):
@@ -144,16 +139,12 @@ class Chain:
                 break
             start_block = self.current_block
 
-    def _return_events_with_retry(
-        self, fetch_events_func, start_block, end_block, step, extra_log
-    ):
+    def _return_events_with_retry(self, fetch_events_func, start_block, end_block, step, extra_log):
         MAX_RETRIES = 3
         retry = 0
         while retry <= MAX_RETRIES:
             try:
                 events = fetch_events_func(start_block, end_block, step)
-                self.current_block = end_block
-                return events
             except ValueError as e:
                 msg = e.args[0]
                 if isinstance(msg, dict) and msg["code"] in [-32602, -32005, -32000]:
@@ -171,18 +162,19 @@ class Chain:
                         **extra_log,
                     }
                     if retry == MAX_RETRIES:
-                        log.error(msg, extra=log_extra)
+                        log.exception(msg, extra=log_extra)
                         return None
                     else:
                         log.warning(msg, extra=log_extra)
                         retry += 1
+            else:
+                self.current_block = end_block
+                return events
         return None
 
     ### Topics
 
-    def _return_events_by_topics(
-        self, contract_address, topics, start_block, end_block, step
-    ):
+    def _return_events_by_topics(self, contract_address, topics, start_block, end_block, step):
         filters = {
             "fromBlock": start_block,
             "toBlock": end_block,
@@ -192,21 +184,13 @@ class Chain:
         events = self.eth.get_logs(filters)
         return events
 
-    def _return_events_by_topic_with_retry(
-        self, contract_address, topics, start_block, end_block, step, extra_log
-    ):
+    def _return_events_by_topic_with_retry(self, contract_address, topics, start_block, end_block, step, extra_log):
         def fetch_events_func(start_block, end_block, step):
-            return self._return_events_by_topics(
-                contract_address, topics, start_block, end_block, step
-            )
+            return self._return_events_by_topics(contract_address, topics, start_block, end_block, step)
 
-        return self._return_events_with_retry(
-            fetch_events_func, start_block, end_block, step, extra_log
-        )
+        return self._return_events_with_retry(fetch_events_func, start_block, end_block, step, extra_log)
 
-    def yield_contract_events_by_topic(
-        self, contract_address, topics, from_block, to_block=None
-    ):
+    def yield_contract_events_by_topic(self, contract_address, topics, from_block, to_block=None):
         contract_address = Web3.to_checksum_address(contract_address)
 
         def fetch_events_func(start_block, end_block, step):
@@ -223,9 +207,7 @@ class Chain:
 
     ### Event Attr
 
-    def _return_contract_events(
-        self, contract, event_attr, start_block, end_block, step
-    ):
+    def _return_contract_events(self, contract, event_attr, start_block, end_block, step):
         attr = contract.events
         cls = getattr(attr, event_attr)
         caller = cls.create_filter
@@ -233,13 +215,9 @@ class Chain:
         entries = events.get_all_entries()
         return entries
 
-    def _return_contract_events_with_retry(
-        self, contract, event_attr, start_block, end_block, step, extra_log
-    ):
+    def _return_contract_events_with_retry(self, contract, event_attr, start_block, end_block, step, extra_log):
         def fetch_events_func(start_block, end_block, step):
-            return self._return_contract_events(
-                contract, event_attr, start_block, end_block, step
-            )
+            return self._return_contract_events(contract, event_attr, start_block, end_block, step)
 
         return self._return_events_with_retry(
             fetch_events_func,
@@ -249,9 +227,7 @@ class Chain:
             extra_log={"contract": contract, "event_attr": event_attr, **extra_log},
         )
 
-    def yield_contract_events(
-        self, contract_address, event_attr, from_block, to_block=None, **kwargs
-    ):
+    def yield_contract_events(self, contract_address, event_attr, from_block, to_block=None, **kwargs):
         abi_address = kwargs.get("abi_type", contract_address)
         contract = self.get_contract(abi_address)
 
