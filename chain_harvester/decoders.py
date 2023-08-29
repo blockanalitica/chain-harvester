d3a3e3abfc4725acaa9f8016d3856e557a24597a
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2023-08-29 10:07:09 +0200

    feat: changed event log decoder output

--- chain_harvester/decoders.py
+++ chain_harvester/decoders.py
@@ -1,30 +1,17 @@
 from eth_utils import event_abi_to_log_topic
-from web3._utils.abi import get_abi_input_names, get_abi_input_types, map_abi_data
-from web3._utils.normalizers import BASE_RETURN_NORMALIZERS
+from web3._utils.events import get_event_data
 
 
 class EventLogDecoder:
     def __init__(self, contract):
         self._contract = contract
-        self._event_abis = [abi for abi in self._contract.abi if abi["type"] == "event"]
-        self._signed_abis = {event_abi_to_log_topic(abi): abi for abi in self._event_abis}
+        event_abis = [abi for abi in self._contract.abi if abi["type"] == "event"]
+        self._signed_abis = {event_abi_to_log_topic(abi): abi for abi in event_abis}
 
-    def decode_log(self, log):
-        data = b"".join(log["topics"] + [log["data"]])
-
-        selector, params = data[:32], data[32:]
-
+    def decode_log(self, log_entry):
+        data = b"".join(log_entry["topics"] + [log_entry["data"]])
+        selector = data[:32]
         func_abi = self._signed_abis[selector]
 
-        names = get_abi_input_names(func_abi)
-        types = get_abi_input_types(func_abi)
-
-        decoded = self._contract.w3.codec.decode(types, params)
-        normalized = map_abi_data(BASE_RETURN_NORMALIZERS, types, decoded)
-
-        decoded_data = dict(zip(names, normalized))
-
-        log = dict(log)
-        log["decoded_data"] = decoded_data
-        log["event_name"] = func_abi["name"]
-        return log
+        event = get_event_data(self._contract.w3.codec, func_abi, log_entry)
+        return event
