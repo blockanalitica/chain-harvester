c42ceeaba334feb138dfd7a8e5a2edb0aaef0995
0xCommanderKeen <91289621+0xCommanderKeen@users.noreply.github.com>
2024-07-30 18:21:14 +0200

    feat: abi to get topics get_topics_for_contract

--- chain_harvester/api.py
+++ chain_harvester/api.py
@@ -19,3 +19,8 @@ def get_events_for_contracts(
     return chain.get_events_for_contracts_topics(
         contract_addresses, topics, from_block, to_block=to_block
     )
+
+
+def get_topics_for_contract(network, contract_address, config=None):
+    chain = get_chain(network, rpc=config["rpc"], api_key=config["etherscan_api_key"])
+    return chain.abi_to_event_topics(contract_address)
