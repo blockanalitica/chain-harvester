186f325537416160a62aaac33c9400037af825de
0xCommanderKeen <91289621+0xCommanderKeen@users.noreply.github.com>
2024-09-09 19:12:01 +0200

    fix

--- intregration_tests/adapters/test_alchemy.py
+++ intregration_tests/adapters/test_alchemy.py
@@ -1,5 +1,4 @@
 from chain_harvester.adapters.alchemy import Alchemy
-from chain_harvester.decoders import EventLogDecoder
 from intregration_tests.env import API_KEYS, RPC_NODES
 
 
@@ -27,21 +26,4 @@ def test_get_transactions_for_contracts__failed():
         failed=True,
     )
     events = list(data)
-    assert len(events) == 0
-
-
-def test_get_transactions_for_contracts__cowswap():
-    alchemy = Alchemy("ethereum", "mainnet", rpc_nodes=RPC_NODES, api_keys=API_KEYS)
-    data = alchemy.get_transactions_for_contracts(
-        ["0x9008D19f58AAbD9eD0D60971565AA8510560ab41"],
-        20427684,
-        to_block=20427684 + 1,
-        failed=False,
-    )
-    events = list(data)
-
-    for log in events[0]["tx"]["logs"]:
-        contract = alchemy.chain.get_contract("0x9008D19f58AAbD9eD0D60971565AA8510560ab41")
-        data = EventLogDecoder(contract).decode_log(log)
-
     assert len(events) == 0
