1307c31cadec665d982f4f9c1cf5f97f82561c97
roks0n <haluzan.rok@gmail.com>
2025-01-31 08:01:05 +0100

    fix: division by zero in rates calc

--- chain_harvester/utils/rates.py
+++ chain_harvester/utils/rates.py
@@ -4,8 +4,14 @@ from chain_harvester.constants import SECONDS_PER_YEAR
 
 
 def calculate_average_rate(previous_index, previous_time, current_index, current_time):
+    if previous_index <= 0:
+        return Decimal("0")
+
     time_difference = current_time - previous_time
     seconds = Decimal(time_difference.total_seconds())
+    if seconds == Decimal(0):
+        return Decimal("0")
+
     period = SECONDS_PER_YEAR / seconds
     return (Decimal(str(current_index)) / Decimal(str(previous_index))) ** period - Decimal("1")
 
