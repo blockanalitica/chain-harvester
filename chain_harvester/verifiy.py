54f9fb2ae6e22e8e11744776dc9be18b53702740
0xCommanderKeen <91289621+0xCommanderKeen@users.noreply.github.com>
2025-04-15 14:24:31 +0200

    fix

--- chain_harvester/verifiy.py
+++ chain_harvester/verifiy.py
@@ -2,10 +2,10 @@ from eth_account import Account
 from eth_account.messages import encode_defunct
 
 
-def verify_ethereum_signature(message, signature, address):
-    """Verify that the message was signed by the Ethereum address."""
+def verify_signature(message, signature, address):
+    """Verify that the message was signed by theaddress."""
     try:
-        # Format the message as an Ethereum specific message
+        # Format the message as an specific message
         message_hash = encode_defunct(text=message)
         # Recover the address from the signature
         recovered_address = Account.recover_message(message_hash, signature=signature)
