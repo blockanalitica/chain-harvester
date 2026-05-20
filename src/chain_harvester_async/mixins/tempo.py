a069dd52fad40f3295e10438046e62a29f0efd85
Aljoša <aljosaurbas@gmail.com>
2026-05-20 11:01:35 +0200

    feat: WIP (#304)

--- src/chain_harvester_async/mixins/tempo.py
+++ src/chain_harvester_async/mixins/tempo.py
@@ -2,7 +2,7 @@ import logging
 
 from chain_harvester.exceptions import ChainException
 from chain_harvester_async.mixins.base import BaseExplorerMixin
-from chain_harvester_async.utils.http import retry_get_json
+from chain_harvester_async.utils.http import retry_get_json, retry_post_json
 
 log = logging.getLogger(__name__)
 
@@ -14,6 +14,7 @@ class TempoMixin(BaseExplorerMixin):
         **kwargs,
     ):
         self.api = "https://contracts.tempo.xyz/v2"
+        self.rpc_url = "https://rpc.tempo.xyz"
         super().__init__(*args, **kwargs)
 
     async def get_abi_from_source(self, contract_address):
@@ -34,3 +35,35 @@ class TempoMixin(BaseExplorerMixin):
 
         abi = data["abi"]
         return abi
+
+    async def get_closest_block_before_timestamp(self, timestamp):
+        url = f"https://coins.llama.fi/block/tempo/{timestamp}"
+        response = await retry_get_json(url)
+        block_number = response["height"]
+        block_ts = response["timestamp"]
+        tries = 0
+
+        while block_ts > timestamp:
+            if tries > 5:
+                log.error(
+                    "Couldn't find closest block before %s. Using %s as closest alternative",
+                    timestamp,
+                    block_number,
+                )
+                break
+            block_number -= 1
+            block = await self._get_block_by_number(hex(block_number))
+            block_ts = int(block["timestamp"], 16)
+            tries += 1
+
+        return block_number
+
+    async def _get_block_by_number(self, block_number):
+        payload = {
+            "jsonrpc": "2.0",
+            "method": "eth_getBlockByNumber",
+            "params": [block_number, False],
+            "id": 1,
+        }
+        response = await retry_post_json(self.rpc_url, json=payload)
+        return response["result"]
